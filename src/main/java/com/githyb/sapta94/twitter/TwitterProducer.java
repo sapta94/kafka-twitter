7ba24499d4910d642f4875d84601507d8b13f92f
sapta94 <dey7.kol@gmail.com>
2020-09-20 12:56:35 +0530

    setup of kafka twitter client

---
+++ src/main/java/com/githyb/sapta94/twitter/TwitterProducer.java
@@ -0,0 +1,78 @@
+package com.githyb.sapta94.twitter;
+
+import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.Hosts;
+import com.twitter.hbc.core.HttpHosts;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.omg.CORBA.TIMEOUT;
+
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class TwitterProducer {
+
+    public TwitterProducer(){}
+
+
+    public static void main(String[] args) {
+        new TwitterProducer().run();
+    }
+
+    public void run(){
+        /** Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(1000);
+
+        // create a twitter client
+        Client client = createTwitterClient(msgQueue);
+
+        //Establish connection
+        client.connect();
+        // create a kafka producer
+
+        //loop to send tweets to kafka
+        while(!client.isDone()){
+            try {
+                String msg = msgQueue.poll(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                client.stop();
+            }
+        }
+    }
+
+    String consumerKey="";
+    String consumerSecret="";
+    String token = "";
+    String tokenSecret = "";
+
+    public Client createTwitterClient(BlockingQueue<String> msgQueue){
+
+        /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
+        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
+        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
+
+        List<String> terms = Lists.newArrayList("twitter", "api");
+        hosebirdEndpoint.trackTerms(terms);
+
+        // These secrets should be read from a config file
+        Authentication hosebirdAuth = new OAuth1("consumerKey", "consumerSecret", "token", "secret");
+
+        ClientBuilder builder = new ClientBuilder()
+                .name("Hosebird-Client-01")                              // optional: mainly for the logs
+                .hosts(hosebirdHosts)
+                .authentication(hosebirdAuth)
+                .endpoint(hosebirdEndpoint)
+                .processor(new StringDelimitedProcessor(msgQueue));
+
+        Client hosebirdClient = builder.build();
+        return hosebirdClient;
+    }
+}
